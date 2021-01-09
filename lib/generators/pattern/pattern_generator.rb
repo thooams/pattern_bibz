d4d8b3657434554912b1d2a070a60c1b5d29ff6f
Thomas HUMMEL <thomas@hummel.link>
2021-01-09 16:35:15 +0100

    Add frozen_string_literal comment

--- lib/generators/pattern/pattern_generator.rb
+++ lib/generators/pattern/pattern_generator.rb
@@ -11,6 +11,8 @@ class PatternGenerator < Rails::Generators::NamedBase
     return if File.exist?(application_file_name)
 
     create_file application_file_name, <<~FILE
+      # frozen_string_literal: true
+
       class Application#{@pattern_value_object.pattern_klass_name}
       end
     FILE
@@ -18,6 +20,8 @@ class PatternGenerator < Rails::Generators::NamedBase
 
   def create_app_pattern_file
     create_file "app/#{@pattern_value_object.directory_name}/#{@pattern_value_object.file_name}.rb", <<~FILE
+      # frozen_string_literal: true
+
       class #{@pattern_value_object.klass_name} < Application#{@pattern_value_object.pattern_klass_name}
       end
     FILE
@@ -29,6 +33,8 @@ class PatternGenerator < Rails::Generators::NamedBase
     return if File.exist?(application_file_name)
 
     create_file application_file_name, <<~FILE
+      # frozen_string_literal: true
+
       require 'test_helper'
 
       class Application#{@pattern_value_object.pattern_klass_name}Test < ActiveSupport::TestCase
@@ -38,6 +44,8 @@ class PatternGenerator < Rails::Generators::NamedBase
 
   def create_test_pattern_file
     create_file "test/#{@pattern_value_object.directory_name}/#{@pattern_value_object.file_name}_test.rb", <<~FILE
+      # frozen_string_literal: true
+
       require 'test_helper'
 
       class #{@pattern_value_object.klass_name}Test < ActiveSupport::TestCase
--- lib/pattern_bibz/version.rb
+++ lib/pattern_bibz/version.rb
@@ -1,3 +1,3 @@
 module PatternBibz
-  VERSION = '1.3.0'
+  VERSION = '1.3.1'
 end
